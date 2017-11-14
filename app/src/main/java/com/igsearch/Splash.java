cf35fb585d2edbdcf8376d00b6dd9c7f41051b0e
Ray Zhou <zhou.ray@gmail.com>
2017-11-14 23:26:30 +0800

    upgraded gradle to 4.1. added splash screen template

---
+++ app/src/main/java/com/igsearch/Splash.java
@@ -0,0 +1,25 @@
+package com.igsearch;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Splash extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)  {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, Login.class);
+
+        startActivity(intent);
+
+        finish();
+
+    }
+
+}
